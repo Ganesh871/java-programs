d7cc767f5f22d45ef204152fd60df232b75d7852
ganesh kumar <113965760+Ganesh871@users.noreply.github.com>
2022-10-14 11:01:16 +0530

    Add files via upload

---
+++ Calculator3.java
@@ -0,0 +1,16 @@
+class Calculator3
+{
+String brand;
+int price;
+{
+System.out.println("no arguements constructor");
+}
+Calculator3()  //no arg(or)zero constructor
+{
+
+}
+public static void main(String[] args)
+{
+Calculator3 calc=new Calculator3();
+}
+}
