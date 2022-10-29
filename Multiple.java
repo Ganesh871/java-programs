ee09407f2df8a34d076b976cff9db298080bfffa
ganesh kumar <113965760+Ganesh871@users.noreply.github.com>
2022-10-29 17:06:15 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,12 @@
+class Multiple implements Printable, Show
+{
+public void print()
+{
+System.out.println("multiple inheritance");
+}
+public static void main(String[] args)
+{
+Multiple m=new Multiple();
+m.print();
+}
+} 
