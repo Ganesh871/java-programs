d9b24debd581590663501de8b43148026994745f
ganesh kumar <113965760+Ganesh871@users.noreply.github.com>
2022-11-01 15:42:56 +0530

    Add files via upload

---
+++ Ifelseifdemo.java
@@ -0,0 +1,19 @@
+class Ifelseifdemo
+{
+public static void main(String[] args)
+{
+int i=-15;
+if(i>0)
+{
+System.out.println("positive number");
+}
+else if(i<0)
+{
+System.out.println("negative number");
+}
+else
+{
+System.out.println("zero");
+}
+}
+}
