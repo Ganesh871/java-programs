76afa2981fed5751ab2569a0c8e5837505eadffa
ganesh kumar <113965760+Ganesh871@users.noreply.github.com>
2022-11-01 15:25:26 +0530

    Add files via upload

---
+++ Ifelsedemo.java
@@ -0,0 +1,11 @@
+class Ifelsedemo
+{
+public static void main(String[] args)
+{
+int i=14;
+if(i<15)
+System.out.println("i is smaller than 15");
+else
+System.out.println("i is greater than 15");
+}
+}
