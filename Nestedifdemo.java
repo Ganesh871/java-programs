7cd19dc23144101608f67edef217508a09140808
ganesh kumar <113965760+Ganesh871@users.noreply.github.com>
2022-11-01 15:49:40 +0530

    Add files via upload

---
+++ Nestedifdemo.java
@@ -0,0 +1,15 @@
+class Nestedifdemo
+{
+public static void main(String[] args)
+{
+int age=20;
+int weight=80;
+if(age>=18)
+{
+if(weight>50)
+{
+System.out.println("you are eligiple to donate blood");
+}
+}
+}
+}
